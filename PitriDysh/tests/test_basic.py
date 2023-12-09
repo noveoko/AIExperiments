5cecd0db429425386388eeccf67181247b2017d1
Marcin Kraszewski <visitwarsaw@gmail.com>
2023-12-09 16:04:01 +0100

    Create test_basic.py

---
+++ PitriDysh/tests/test_basic.py
@@ -0,0 +1,33 @@
+ import unittest
+
+class TestSimulation(unittest.TestCase):
+    def setUp(self):
+        # Initialize with test data
+        self.shapes = ['test_shape1.svg', 'test_shape2.svg']
+        self.space = pymunk.Space()
+
+    def test_load_svg_shapes(self):
+        loaded_shapes = load_svg_shapes(self.shapes, self.space)
+        self.assertTrue(len(loaded_shapes) > 0, "Shapes should be loaded")
+
+    def test_initialize_physics(self):
+        # Assuming we have a test shape loaded
+        loaded_shapes = load_svg_shapes(self.shapes, self.space)
+        initialize_physics(loaded_shapes, self.space)
+        # Test for a specific physical property, like mass
+        for shape in loaded_shapes:
+            self.assertTrue(shape.body.mass > 0, "Mass should be set")
+
+    def test_update_physics(self):
+        # This test might be more complex as it requires checking the state change
+        pass
+
+    def test_video_output(self):
+        main(self.shapes)
+        # Check if the video file is created
+        import os
+        self.assertTrue(os.path.isfile('simulation.mp4'), "Video file should be created")
+
+if __name__ == '__main__':
+    unittest.main()
+
